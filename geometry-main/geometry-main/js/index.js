0ee230581a09f113965ab90ce9b8a0dbb633ec96
emon3455 <emon.mhk69@gmail.com>
2023-02-18 21:35:41 +0600

    project done

--- geometry-main/geometry-main/js/index.js
+++ geometry-main/geometry-main/js/index.js
@@ -1,20 +1,133 @@
 
-// for triangle:
-const calculateBtn = document.getElementById('triangle-calculate-btn');
-calculateBtn.addEventListener('click', () => {
-  const baseInput = document.getElementById('triangle-base-input');
-  const heightInput = document.getElementById('triangle-height-input');
-  const areaOutput = document.getElementById('triangle-area-output');
+// initializing the counter
+let count = 0;
 
-  const base = parseFloat(baseInput.value);
-  const height = parseFloat(heightInput.value);
 
-  const area = 0.5 * base * height;
+// Triangle
+document.getElementById("triangle-calc-btn").addEventListener("click" ,  function(){
 
-  if (!isNaN(area)) {
-    areaOutput.textContent = area.toFixed(2);
-  } else {
-    areaOutput.textContent = 'Invalid input';
+  const triangleBase = getInputValueByID("triangle-b-input");
+  const triangleHeight = getInputValueByID("triangle-h-input");
+
+  if(isNaN(triangleBase) || isNaN(triangleHeight) || triangleBase < 0 || triangleHeight < 0){
+      alert("please enter an valid input");
+      return;
   }
-  console.log(area)
+
+  const triangleArea = (0.5 * triangleBase * triangleHeight).toFixed(2);
+
+  const name = getElementValueByID("triangle-name");
+
+  count = count +1;
+
+  showInfo(count , name, triangleArea);
+
 });
+
+// Rectangle
+document.getElementById("rectangle-calc-btn").addEventListener("click", function(){
+
+  const rectangleWidth = getInputValueByID("rectangle-w-input");
+  const rectangleLength = getInputValueByID("rectangle-l-input");
+
+  if(isNaN(rectangleWidth) || isNaN(rectangleLength) || rectangleWidth < 0 || rectangleLength < 0){
+      alert("please enter an number(possitive)");
+      return;
+  }
+
+  const rectangleArea = (rectangleWidth * rectangleLength).toFixed(2);
+
+  const name = getElementValueByID("rectangle-name");
+  count = count + 1;
+
+  showInfo(count , name, rectangleArea);
+
+});
+
+// paralelogram:
+document.getElementById("paralelogram-calc-btn").addEventListener("click" , function(){
+
+  const base = getElementNumberValueByID("paralel0-b");
+  const height = getElementNumberValueByID("paralelo-h");
+
+  const area = (base * height).toFixed(2);
+
+  const name = getElementValueByID("paralelo-name");
+
+  count = count +1;
+
+  showInfo(count , name, area);
+
+});
+
+
+// Rhombus:
+document.getElementById("rhombus-calc-btn").addEventListener("click" , function(){
+
+  const d1 = getElementNumberValueByID("rhombus-data1");
+  const d2 = getElementNumberValueByID("rhombus-data2");
+  const area = (0.5 * d1 * d2).toFixed(2);
+
+  const name = getElementValueByID("rombos-name");
+
+  count = count +1;
+  showInfo(count , name, area);
+
+});
+
+
+
+// Pentagon:
+document.getElementById("pentagon-calc-btn").addEventListener("click" , function(){
+
+  const peremeter = getElementNumberValueByID("pentagon-p");
+  const apothem = getElementNumberValueByID("pentagon-b");
+  const area = (0.5 * peremeter * apothem).toFixed(2);
+
+  const name = getElementValueByID("pentagon-name");
+
+  count = count +1;
+  showInfo(count , name, area);
+
+});
+
+
+// Ellipse:
+document.getElementById("ellipse-calc-btn").addEventListener("click" , function(){
+
+  const a = getElementNumberValueByID("ellip-a");
+  const b = getElementNumberValueByID("ellip-b");
+  const pie = 3.14;
+  const area = (pie * a * b).toFixed(2);
+
+  const name = getElementValueByID("ellip-name");
+
+  count = count +1;
+  showInfo(count , name, area);
+
+});
+
+
+// generating random color:
+function generateRGBColor(){
+
+  var first = Math.floor(Math.random() * 256);
+  var sec = Math.floor(Math.random() * 256);
+  var third = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + first + "," + sec + "," + third + ")";
+
+  return bgColor;
+
+}
+
+
+// set the bg color on card background
+const allCards = document.getElementById("cards").children;
+console.log(allCards);
+for (const card of allCards) {
+    card.addEventListener("mouseover" , function(){
+        const bgColor = generateRGBColor();
+        card.style.backgroundColor = bgColor;
+    });
+}
+
--- geometry-main/geometry-main/js/utilities.js
+++ geometry-main/geometry-main/js/utilities.js
@@ -1,10 +1,45 @@
-//  aikhane sob common function gulo likhe raikho jmn:
+// common functions start:
 
-// 1: input field gulo theke data neya
+// get number type data from input
+function getInputValueByID(elementId){
+    const inputF = document.getElementById(elementId);
+    const inputV = parseFloat(inputF.value);
 
-// 2: html element theke data neya:
+    inputF.value = "";
 
-// 3: html element theke nmbr type data neya
+    return inputV;
+}
 
-// 4: button gulo te click korle sob value gulo set kore deya
+// get number type data from element
+function getElementNumberValueByID(elementId){
+   const htmlElement = document.getElementById(elementId);
+   const values = parseFloat(htmlElement.innerText);
+
+   return values;
+}
+// to get the element value from text field
+function getElementValueByID(elementId){
+   const htmlElement = document.getElementById(elementId);
+   const values = htmlElement.innerText;
+
+   return values;
+}
+
+//  to display on the table
+function showInfo(count, name, areas){
+    let tBody = document.getElementById("tBody");
+    let tRow  = document.createElement("tr");
+    tRow.innerHTML=`
+    
+    <td> ${count}.  ${name} </td>
+    <td> ${areas}cm<sup>2</sup> </td>
+    <td> 
+     <button class=" bg-sky-600 border-none p-2 text-white rounded-lg hover:bg-sky-800 ">Convert To cm<sup>2</sup>
+     </button>
+    </td> `;
+    
+    tBody.appendChild(tRow);
+
+}
+
 
